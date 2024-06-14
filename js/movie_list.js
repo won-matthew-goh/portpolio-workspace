a2ae8c25397879b956227e278dc85bf19a5122a2
kkmiini <vuy100@naver.com>
2024-06-14 15:46:37 +0900

    modal추가

---
+++ js/movie_list.js
@@ -0,0 +1,10 @@
+var modalwp = document.getElementById('MyModal');
+
+function modal(){
+    
+    modalwp.innerHTML = '<h1>제목입니다</h1>'
+    + '<p>내용입니다.</p>'
+    + '<div>내용입니다.</div>'
+    + '<div>내용입니다.</div>'
+    + '<button onclick="">닫기</button>';
+};
