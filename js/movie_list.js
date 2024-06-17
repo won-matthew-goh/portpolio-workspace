f7fa59df74552bb6156f4a2fd62701ea8443ee0a
kkmiini <vuy100@naver.com>
2024-06-17 18:13:29 +0900

    modal 업데이트

--- js/movie_list.js
+++ js/movie_list.js
@@ -1,10 +1,13 @@
-var modalwp = document.getElementById('MyModal');
+const movie = document.querySelector(".movie");
+const modalwp = document.querySelector(".modal-wp");
 
-function modal(){
-    
-    modalwp.innerHTML = '<h1>제목입니다</h1>'
-    + '<p>내용입니다.</p>'
-    + '<div>내용입니다.</div>'
-    + '<div>내용입니다.</div>'
-    + '<button onclick="">닫기</button>';
-};
+movie.addEventListener("click", function () {
+  //display 속성을 block로 변경
+  modalwp.style.display = "flex";
+});
+
+const modalclose = document.querySelector(".modal_close");
+
+modalclose.addEventListener("click", function () {
+  modalwp.style.display = "none";
+});
