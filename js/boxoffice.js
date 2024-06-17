95f8107c8e3e088ae43ac9378225136c28263f80
won-matthew-goh <qimo2000@naver.com>
2024-06-17 23:26:19 +0900

    index carousel jquery 제거 vanila javascript로 전환 Boxoffice chart.js 적용 테스트

--- js/boxoffice.js
+++ js/boxoffice.js
@@ -17,12 +17,19 @@ let accChart = new Chart(accCt, {
 let weekChart = new Chart(weekCt, {
   type: 'bar',
   data: {
+    axis: 'y',
     labels: ['원더랜드', '퓨리오사: 매드맥스 사가', '그녀가 죽었다', '범죄도시4', '설계자'],
     datasets: [
       {
         label: '관객수 증감',
         data: [460191, -118731, -61083, -79408, -240543],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)'],
+        borderWidth: 1,
       },
     ],
+  },
+  options: {
+    indexAxis: 'y',
   },
 });
--- js/index.js
+++ js/index.js
@@ -1,24 +1,93 @@
 AOS.init({ duration: 1200 });
 
-/********* Jquery 이용한 Carousels *******/
+// /********* Jquery 이용한 Carousels *******/
+// /*************** global init **************/
+// var $wrapper = $('.banner-wrapper');
+// var $slide = $wrapper.find('.banner-slide');
+// var $pager = $wrapper.find('.pager');
+// var $btPrev = $wrapper.find('.prevBtn');
+// var $btNext = $wrapper.find('.nextBtn');
+// var interval; // setInterval을 넣어놓을 변수
+// var idx = 0; // animation이 움직일 값의 인자 0, -100%, -200% ...
+// var gap = 3000; // setInterval의 간격
+// var speed = 500; // animation speed
+// var depth = 1; // z-index
+// var last = $slide.find('.slide-item').length - 1; // $('.slide')의 마지막 index
+
+// /*************** user function **************/
+// function ani() {
+//   $slide.stop().animate({ left: -(idx * 100) + '%' }, speed);
+//   $pager.removeClass('active');
+//   $pager.eq(idx === last ? 0 : idx).addClass('active');
+// }
+
+// /*************** event callback *************/
+// function onEnter() {
+//   clearInterval(interval);
+// }
+
+// function onLeave() {
+//   interval = setInterval(onInterval, gap);
+// }
+
+// function onInterval() {
+//   interval = setInterval(onNext, gap);
+// }
+
+// function onPager() {
+//   idx = $(this).index();
+//   ani();
+// }
+
+// function onPrev() {
+//   // idx = idx === 0 ? last - 1 : idx - 1;
+//   if (idx === 0) {
+//     $slide.css('left', -last * 100 + '%');
+//     idx = last;
+//   }
+//   idx--;
+//   ani();
+// }
+
+// function onNext() {
+//   if (idx === last) {
+//     $slide.css('left', 0);
+//     idx = 0;
+//   }
+//   idx++;
+//   ani();
+// }
+
+// /*************** event init *****************/
+// // $wrapper.mouseenter(onEnter).mouseleave(onLeave);
+// $pager.click(onPager);
+// $btPrev.click(onPrev);
+// $btNext.click(onNext);
+
+// /*************** start init *****************/
+// interval = setInterval(onNext, gap);
+
+/********* JavaScript로 변환된 Carousels *******/
 /*************** global init **************/
-var $wrapper = $('.banner-wrapper');
-var $slide = $wrapper.find('.banner-slide');
-var $pager = $wrapper.find('.pager');
-var $btPrev = $wrapper.find('.prevBtn');
-var $btNext = $wrapper.find('.nextBtn');
+var wrapper = document.querySelector('.banner-wrapper');
+var slide = wrapper.querySelector('.banner-slide');
+var pager = wrapper.querySelectorAll('.pager');
+var btPrev = wrapper.querySelector('.prevBtn');
+var btNext = wrapper.querySelector('.nextBtn');
 var interval; // setInterval을 넣어놓을 변수
 var idx = 0; // animation이 움직일 값의 인자 0, -100%, -200% ...
 var gap = 3000; // setInterval의 간격
 var speed = 500; // animation speed
-var depth = 1; // z-index
-var last = $slide.find('.slide-item').length - 1; // $('.slide')의 마지막 index
+var last = slide.querySelectorAll('.slide-item').length - 1; // ('.slide-item')의 마지막 index
 
 /*************** user function **************/
 function ani() {
-  $slide.stop().animate({ left: -(idx * 100) + '%' }, speed);
-  $pager.removeClass('active');
-  $pager.eq(idx === last ? 0 : idx).addClass('active');
+  slide.style.transition = `left ${speed}ms ease`;
+  slide.style.left = -(idx * 100) + '%';
+  pager.forEach(function (el) {
+    el.classList.remove('active');
+  });
+  pager[idx === last ? 0 : idx].classList.add('active');
 }
 
 /*************** event callback *************/
@@ -27,42 +96,46 @@ function onEnter() {
 }
 
 function onLeave() {
-  interval = setInterval(onInterval, gap);
+  interval = setInterval(onNext, gap);
 }
 
 function onInterval() {
   interval = setInterval(onNext, gap);
 }
 
-function onPager() {
-  idx = $(this).index();
+function onPager(event) {
+  idx = Array.from(pager).indexOf(event.target);
   ani();
 }
 
 function onPrev() {
-  // idx = idx === 0 ? last - 1 : idx - 1;
   if (idx === 0) {
-    $slide.css('left', -last * 100 + '%');
+    slide.style.left = -last * 100 + '%';
     idx = last;
+  } else {
+    idx--;
   }
-  idx--;
   ani();
 }
 
 function onNext() {
   if (idx === last) {
-    $slide.css('left', 0);
+    slide.style.left = 0;
     idx = 0;
+  } else {
+    idx++;
   }
-  idx++;
   ani();
 }
 
 /*************** event init *****************/
-// $wrapper.mouseenter(onEnter).mouseleave(onLeave);
-$pager.click(onPager);
-$btPrev.click(onPrev);
-$btNext.click(onNext);
+// wrapper.addEventListener('mouseenter', onEnter);
+// wrapper.addEventListener('mouseleave', onLeave);
+pager.forEach(function (el) {
+  el.addEventListener('click', onPager);
+});
+btPrev.addEventListener('click', onPrev);
+btNext.addEventListener('click', onNext);
 
 /*************** start init *****************/
 interval = setInterval(onNext, gap);
