269edefb53b54f51f0bbf5f59948d9458c45b600
mini <vuy100@naver.com>
2024-06-19 01:17:30 +0900

    modal 업데이트

--- js/movie_list.js
+++ js/movie_list.js
@@ -1,13 +1,16 @@
 const movie = document.querySelector(".movie");
 const modalwp = document.querySelector(".modal-wp");
+const modalbackground = document.querySelector(".modal-background");
 
 movie.addEventListener("click", function () {
   //display 속성을 block로 변경
   modalwp.style.display = "flex";
+  modalbackground.style.display = "flex";
 });
 
 const modalclose = document.querySelector(".modal_close");
 
 modalclose.addEventListener("click", function () {
   modalwp.style.display = "none";
+  modalbackground.style.display = "none";
 });
