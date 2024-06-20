74a217d9b824588f72d3e8d5fc590a4a66a5c83b
won-matthew-goh <qimo2000@naver.com>
2024-06-20 19:53:18 +0900

    movie_list 퍼블리싱 완성

--- js/movie_list.js
+++ js/movie_list.js
@@ -2,30 +2,37 @@ AOS.init({});
 
 /********** modal 열기 **********/
 
-const modalwrap = document.querySelector(".modal-wrap");
-const modalwrapper = document.querySelector(".modal-wrapper");
-const movie = document.querySelectorAll(".movie");
+const modalwrap = document.querySelector('.modal-wrap');
+const modalwrapper = document.querySelector('.modal-wrapper');
+const movie = document.querySelectorAll('.movie');
+const modalBg = document.querySelector('.modal-bg');
 
 movie.forEach(function (a) {
-  a.addEventListener("click", function () {
-    modalwrap.style.display = "block";
-    modalwrapper.style.display = "block";
+  a.addEventListener('click', function () {
+    modalwrap.style.display = 'block';
+    modalwrapper.style.display = 'block';
+    modalBg.style.display = 'block';
   });
 });
 
 /********** modal 닫기 **********/
-const btn = document.querySelector(".btn");
-const closebtn = document.querySelector(".close-btn");
+const btn = document.querySelector('.btn');
+const closebtn = document.querySelector('.close-btn');
 
-btn.addEventListener("click", function () {
+btn.addEventListener('click', function () {
   close();
 });
 
-closebtn.addEventListener("click", function () {
+closebtn.addEventListener('click', function () {
+  close();
+});
+
+modalBg.addEventListener('click', function () {
   close();
 });
 
 function close() {
-  modalwrap.style.display = "none";
-  modalwrapper.style.display = "none";
+  modalwrap.style.display = 'none';
+  modalwrapper.style.display = 'none';
+  modalBg.style.display = 'none';
 }
