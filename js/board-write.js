8888348c4e643de7976abec7ce2d62c9c7d70d7e
ygk <ygk5192@gmail.com>
2024-06-23 22:41:38 +0900

    board-write 별점 추가 수정

--- js/board-write.js
+++ js/board-write.js
@@ -1,49 +1,87 @@
 /********board-write*********/
+/********새로운 별점 매기기 스크립트*******/
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
+
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+  const starClassActive = "rating__star fas fa-star";
+  const starClassUnactive = "rating__star far fa-star";
+  const starsLength = stars.length;
+  let i;
+  stars.map((star) => {
+    star.onclick = () => {
+      i = stars.indexOf(star);
+
+      if (star.className.indexOf(starClassUnactive) !== -1) {
+        printRatingResult(result, i + 1);
+        for (i; i >= 0; --i) stars[i].className = starClassActive;
+      } else {
+        printRatingResult(result, i);
+        for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+      }
+    };
+  });
+}
+
+function printRatingResult(result, num = 0) {
+  result.textContent = `${num}/5`;
+}
+
+executeRating(ratingStars, ratingResult);
+
+/////
 
 const rateWrap = document.querySelectorAll(".rating"),
-  label = document.querySelectorAll(".rating.rating-half");
-input = document.querySelectorAll(".rating.rating-input");
-(labelLength = label.length), (opacityHover = "0.5");
+  label = document.querySelectorAll(".rating.rating-label"),
+  input = document.querySelectorAll(".rating.rating-input"),
+  labelLength = label.length,
+  opacityHover = "0.5";
 
 let stars = document.querySelectorAll(".rating.star-icon");
 
 checkedRate();
 
-rateWrap.forEach((wrap) => {
-  wrap.addEventListener("mouseenter", () => {
-    stars = wrap.querySelectorAll(".star-icon");
+rateWrap.forEach((rating) => {
+  rating.addEventListener("mouseenter", () => {
+    stars = rating.querySelectorAll(".star-icon"); //star-icon class 잡아서 stars에 담기
 
     stars.forEach((starIcon, idx) => {
       starIcon.addEventListener("mouseenter", () => {
-        if (wrap.classList.contains("readonly") == false) {
+        if (rating.classList.contains("readonly") == false) {
           initStars();
           filledRate(idx, labelLengh);
 
           for (let i = 0; i < stars.length; i++) {
             if (stars[i].classList.contains("filled")) {
-              stars[i].computedStyleMap.opacity = opacityHover;
+              stars[i].computedStyleMap.opacity = opacityHover; // computedStyleMap 제대로 알아보기
             }
           }
         }
-      });
+      }); //addEventListner mouseenter 시 function
+
       starIcon.addEventListener("mouseleave", () => {
-        if (wrap.classList.contains("readonly") == false) {
+        if (rating.classList.contains("readonly") == false) {
           starIcon.style.opacity = "1";
           checkedRate();
         }
-      });
-      
-      wrap.addEventListener("mouseleave", () => {
-        if (wrap.classList.contains("readonly")) {
-          e.preventDefault();
+      }); //addEventListner mouseleave 시 function
+
+      rating.addEventListener("mouseleave", () => {
+        if (rating.classList.contains("readonly") == false) {
+          starIcon.style.opacity = "1";
         }
       });
 
+      rating.addEventListener("click", (e) => {
+        if (rating.classList.contains("readonly")) {
+          e.preventDefault();
+        }
+      });
     });
   });
 });
-
-
 
 /******function****** */
 function filledRate(index, length) {
@@ -54,10 +92,10 @@ function filledRate(index, length) {
   }
 }
 
+//funtion 구간
+//checkRate = 선택된 라디오 버튼 이하 인덱스 별점 활성화
 function checkedRate() {
-  let checkedRadio = document.querySelectorAll(
-    '.rating input[type = "radio"]:checked'
-  );
+  let checkedRadio = document.querySelectorAll('.rating input[type = "radio"]:checked');
 
   initStars();
   checkedRadio.forEach((radio) => {
@@ -66,6 +104,7 @@ function checkedRate() {
     for (let i = 0; i < previousSiblings.length; i++) {
       previousSiblings[i].querySelector(".star-icon").classList.add("filled");
     }
+
     radio.nextElementSibling.classList.add("filled");
 
     function prevAll() {
@@ -76,12 +115,12 @@ function checkedRate() {
         radioSiblings.push(prevSibling);
         prevSibling = prevSibling.previousElementSibling;
       }
-      return radioSiblings;
+      return radioSiblings; //이 부분 이해 필요
     }
   });
 }
 
-/*******별점 초기화********/
+/********별점 초기화*********/
 function initStars() {
   for (let i = 0; i < stars.length; i++) {
     stars[i].classList.remove("filled");
