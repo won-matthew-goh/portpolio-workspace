300dd7e52c025e5dc5a4b83185948772fbdef975
won-matthew-goh <qimo2000@naver.com>
2024-07-02 10:34:24 +0900

    ReviewBoardVO null가져오던 문제 수정

--- moviego/src/main/java/com/global/moviego/domain/ReviewBoardVO.java
+++ moviego/src/main/java/com/global/moviego/domain/ReviewBoardVO.java
@@ -3,85 +3,85 @@ package com.global.moviego.domain;
 import java.time.LocalDateTime;
 
 public class ReviewBoardVO {
-	private int user_id;
+	private int userId;
 	
 	private int rating;
 	
 	private String username;
 	
-	private String movie_id;
+	private String movieId;
 	
-	private String movie_nm;
+	private String movieNm;
 	
-	private String poster_url;
+	private String posterUrl;
 	
 	private String comment;
 		
-	private LocalDateTime created_at;
+	private LocalDateTime createdAt;
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getMovieId() {
+    return movieId;
+  }
+
+  public void setMovieId(String movieId) {
+    this.movieId = movieId;
+  }
+
+  public String getMovieNm() {
+    return movieNm;
+  }
+
+  public void setMovieNm(String movieNm) {
+    this.movieNm = movieNm;
+  }
+
+  public String getPosterUrl() {
+    return posterUrl;
+  }
+
+  public void setPosterUrl(String posterUrl) {
+    this.posterUrl = posterUrl;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 	
-	public int getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-
-	public int getRating() {
-		return rating;
-	}
-
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getMovie_id() {
-		return movie_id;
-	}
-
-	public void setMovie_id(String movie_id) {
-		this.movie_id = movie_id;
-	}
-
-	public String getMovie_nm() {
-		return movie_nm;
-	}
-
-	public void setMovie_nm(String movie_nm) {
-		this.movie_nm = movie_nm;
-	}
-
-	public String getPoster_url() {
-		return poster_url;
-	}
-
-	public void setPoster_url(String poster_url) {
-		this.poster_url = poster_url;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public LocalDateTime getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(LocalDateTime created_at) {
-		this.created_at = created_at;
-	}
-
 
 }
