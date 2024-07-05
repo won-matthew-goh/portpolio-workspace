e71b6b8c0bfeadc2ef419cd7dc4efd652ac79794
won-matthew-goh <qimo2000@naver.com>
2024-07-05 12:57:47 +0900

    review Controller Map import 주입

--- moviego/src/main/java/com/global/moviego/controller/ReviewBoardController.java
+++ moviego/src/main/java/com/global/moviego/controller/ReviewBoardController.java
@@ -1,6 +1,7 @@
 package com.global.moviego.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
