cfa2e74456735b106b01c9e11f1c65c56d6862dd
won-matthew-goh <qimo2000@naver.com>
2024-07-06 08:56:10 +0900

    비밀번호 미일치시 회원등록 방지 구현, 회원 가입 구현 완료

--- moviego/src/main/resources/static/js/memberjoin.js
+++ moviego/src/main/resources/static/js/memberjoin.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const errorMsg = '${error}';
-  const noticeUsername = document.querySelector('.alert-duplicate-username');
-  const noticeEmail = document.querySelector('.alert-duplicate-email');
-  const noticePassword = document.querySelector('.alert-psswd-txt');
+    const errorMsg = '${error}';
+    const noticeUsername = document.querySelector('.alert-duplicate-username');
+    const noticeEmail = document.querySelector('.alert-duplicate-email');
+    const noticePassword = document.querySelector('.alert-psswd-txt');
+    const pwInput = document.querySelector('.pw-input');
+    const pwValiInput = document.querySelector('.pw-vali-input');
+    const btSubmit = document.querySelector('.bt-submit');
 
-  if (errorMsg && errorMsg !== '') {
-    if (errorMsg.includes('username')) {
-      noticeUsername.style.display = 'inline-block';
+    if (errorMsg && errorMsg !== '') {
+        if (errorMsg.includes('username')) {
+            noticeUsername.style.display = 'inline-block';
+        }
+        if (errorMsg.includes('email')) {
+            noticeEmail.style.display = 'inline-block';
+        }
     }
-    if (errorMsg.includes('email')) {
-      noticeEmail.style.display = 'inline-block';
-    }
-  }
 
-  const pwInput = document.querySelector('.pw-input');
-  const pwValiInput = document.querySelector('.pw-vali-input');
-  const btSubmit = document.querySelector('.bt-submit');
+    const validatePasswords = () => {
+        if (pwInput.value !== pwValiInput.value) {
+            noticePassword.style.display = 'inline-block';
+            btSubmit.disabled = true; // Disable submit button
+        } else {
+            noticePassword.style.display = 'none';
+            btSubmit.disabled = false; // Enable submit button
+        }
+    };
 
-  pwValiInput.addEventListener('input', () => {
-    if (pwInput.value !== pwValiInput.value) {
-      btSubmit.addEventListener('click'), (e) => {
-        e.preventDefatult;
-      }
-      noticePassword.style.display = 'inline-block';
-    } else {
-      noticePassword.style.display = 'none';
-    }
-  });
+    pwInput.addEventListener('input', validatePasswords);
+    pwValiInput.addEventListener('input', validatePasswords);
 });
-
