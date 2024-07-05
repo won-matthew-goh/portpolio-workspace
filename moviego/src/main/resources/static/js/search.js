9f3a60246cadc5114682b9526c238fa9ed6d7116
won-matthew-goh <qimo2000@naver.com>
2024-07-05 09:49:46 +0900

    Search - Enter키로 작동할 수 있도록 수정

--- moviego/src/main/resources/static/js/search.js
+++ moviego/src/main/resources/static/js/search.js
@@ -1,25 +1,25 @@
 let currentquery = "";
 
 const fetchSearch = async (query) => {
-	try {
-		const response = await fetch(`/movieList/search?query=${encodeURIComponent(query)}`);
-		const data = await response.json();
-		console.log(data.movies);
-		return data.movies;
-	} catch (error) {
-		console.error('Error fetching movies:', error);
-		return [];
-	}
+  try {
+    const response = await fetch(`/movieList/search?query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    console.log(data.movies);
+    return data.movies;
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    return [];
+  }
 };
 
 const SearchMovies = (movies) => {
-	const moviebox = document.querySelector('.content-wrap .box');
-	const contentwrap = document.querySelector('.content-wrap');
-	moviebox.innerHTML = ''; // 이전 검색 결과 초기화
+  const moviebox = document.querySelector('.content-wrap .box');
+  const contentwrap = document.querySelector('.content-wrap');
+  moviebox.innerHTML = ''; // 이전 검색 결과 초기화
 
-	movies.forEach((movie) => {
-		const movieJSON = encodeURIComponent(JSON.stringify(movie));
-		const movieHTML = `
+  movies.forEach((movie) => {
+    const movieJSON = encodeURIComponent(JSON.stringify(movie));
+    const movieHTML = `
       <div class="movie" data-aos="flip-up" data-movie="${movieJSON}" onclick="openModal(this)">
         <img src="https://image.tmdb.org/t/p/w200${movie.posterPath}" width="225" height="337.5"/>
         <div class="movie-name">${movie.title}</div>
@@ -27,30 +27,37 @@ const SearchMovies = (movies) => {
         <div class="nation">${translateNation(movie.originalLanguage)}</div>
       </div>
     `;
-		moviebox.innerHTML += movieHTML;
-	});
+    moviebox.innerHTML += movieHTML;
+  });
 
-	document.removeEventListener('scroll', handleScroll);
+  document.removeEventListener('scroll', handleScroll);
 
-	if (movies.length <= 10 && movies.length > 0) {
-		contentwrap.style.height = '88.7vh';
-		contentwrap.style.padding = '0';
-	} else if (movies.length == 0) {
-		document.location.href = '/movieList';
-	} else {
-		contentwrap.style.height = '';
-		contentwrap.style.padding = '0 0 180px 0';
-	}
+  if (movies.length <= 10 && movies.length > 0) {
+    contentwrap.style.height = '88.7vh';
+    contentwrap.style.padding = '0';
+  } else if (movies.length == 0) {
+    document.location.href = '/movieList';
+  } else {
+    contentwrap.style.height = '';
+    contentwrap.style.padding = '0 0 180px 0';
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const searchButton = document.getElementById('searchButton');
-	const searchInput = document.getElementById('searchInput');
+  const searchButton = document.getElementById('searchButton');
+  const searchInput = document.getElementById('searchInput');
 
-	searchButton.addEventListener('click', async () => {
-		currentquery = searchInput.value;
-		const movies = await fetchSearch(currentquery);
-		console.log('a');
-		SearchMovies(movies);
-	});
+  async function serachEvent() {
+    currentquery = searchInput.value;
+    const movies = await fetchSearch(currentquery);
+    console.log('a');
+    SearchMovies(movies);
+  }
+
+  searchButton.addEventListener('click', serachEvent);
+  searchInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      serachEvent();
+    }
+  });
 });
