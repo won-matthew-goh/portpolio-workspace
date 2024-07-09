fc04f545d7d7dbe8d4b3fe3481c11fcefd791976
won-matthew-goh <qimo2000@naver.com>
2024-07-09 11:25:48 +0900

    boxoffice multilingual 구현 완료

--- moviego/src/main/resources/static/js/boxoffice.js
+++ moviego/src/main/resources/static/js/boxoffice.js
@@ -1,21 +1,42 @@
 let weekCt = document.getElementById('weekChart');
 
 let weekChart = new Chart(weekCt, {
-  	  type: 'bar',
-  	  data: {
-  	    axis: 'y',
-  	    labels: [movieArray[0].movieNm, movieArray[1].movieNm, movieArray[2].movieNm, movieArray[3].movieNm, movieArray[4].movieNm],
-  	    datasets: [
-  	      {
-  	        label: '관객수 증감',
-  	        data: [movieArray[0].audiInten, movieArray[1].audiInten, movieArray[2].audiInten, movieArray[3].audiInten, movieArray[4].audiInten],
-  	        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-  	        borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)'],
-  	        borderWidth: 1,
-  	      },
-  	    ],
-  	  },
-  	  options: {
-  	    indexAxis: 'y',
-  	  },
-  	});
+    type: 'bar',
+    data: {
+        axis: 'y',
+        labels: movieArray.slice(0, 5).map(movie => `${movie.movieNm} / ${movie.titleEn} / ${movie.titleJa}`),
+        datasets: [
+            {
+                label: '관객수 증감 / Audience Change / 観客数の変化',
+                data: movieArray.slice(0, 5).map(movie => movie.audiInten),
+                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+                borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)'],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        indexAxis: 'y',
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = context.dataset.label || '';
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.x !== null) {
+                            label += context.parsed.x;
+                        }
+                        return label;
+                    },
+                    title: function(tooltipItems) {
+                        return movieArray[tooltipItems[0].dataIndex].movieNm + ' / ' + 
+                               movieArray[tooltipItems[0].dataIndex].titleEn + ' / ' + 
+                               movieArray[tooltipItems[0].dataIndex].titleJa;
+                    }
+                }
+            }
+        }
+    },
+});
